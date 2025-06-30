b10bc97f6416b5a6a700feb7c14409f6901d34f6
Jina Ham <hamjina61@gmail.com>
2025-06-30 13:34:39 +0900

    [chore] 초기 프로젝트 셋팅

---
+++ src/main/java/com/example/Premind_BE/PremindBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Premind_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremindBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremindBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Premind_BE/PremindBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Premind_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremindBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
